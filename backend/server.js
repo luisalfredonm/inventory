8c82a818a87cd40842c0187d18f427024000ce3d
Luis Nunez <lualfredo@gmail.com>
2023-11-14 22:17:15 -0600

    starter-project

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+
+const app = express()
+
+
+const PORT = process.env.PORT|| 5000;
+
+
+// conect to DB and Sgart server
+mongoose
+    .connect(process.env.MONGO_URI)
+     .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+     })
+     .catch((err)=>console.error(err))
+
+
